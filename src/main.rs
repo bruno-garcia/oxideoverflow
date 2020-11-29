079670eca756223147d8cdcf065a60e3982946d8
Bruno Garcia <bruno@brunogarcia.com>
2020-11-29 14:42:09 -0500

    get questions within interval

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _guard = sentry::init("https://0fe0d16e158146279a751bbf675f2610@o117736.ingest.sentry.io/5536978");
 
     let interval: Duration = Duration::from_secs(15);
-    let now = SystemTime::now();
 
-    let query = "[sentry]";
+    let tag = "sentry";
+    let max_items = 3;
     let key = env::var("OXIDEOVERFLOW_STACKOVERFLOW_KEY").unwrap();
 
+    let mut offset: Option<Duration> = None;
     loop {
-        let from = now.duration_since(SystemTime::UNIX_EPOCH)?;
+        let now = SystemTime::now();
+        let from = match offset {
+            // From the last run's 'end' timestamp
+            Some(b) => b,
+            // From now
+            None => now.duration_since(SystemTime::UNIX_EPOCH)?
+        };
         let to = now.checked_add(interval).unwrap().duration_since(SystemTime::UNIX_EPOCH)?;
-        let stackoverflow_url = get_url(from, to, query, key.as_str());
+        offset = Some(to);
+        let stackoverflow_url = get_url(&from, &to, tag, max_items, key.as_str());
+        println!("Waiting for {} seconds before polling.", interval.as_secs());
         task::sleep(interval).await;
 
         println!("Fetching from {}", stackoverflow_url);
@@ -27,8 +36,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 if response.status() == 200 {
                     match response.json().await {
                         Ok(r) => {
-                            let json: Response = r;
-                            println!("json: {:#?}", json);
+                            let response: Response = r;
+                            println!("Response: {:#?}", response);
+                            handle_response(response);
                         },
                         Err(e) => println!("err {}", e),
                     };
@@ -41,21 +51,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 }
 
-fn get_url(from: Duration, to: Duration, query: &str, key: &str) -> String {
+fn handle_response(response: Response) {
+    // https://discord.com/api/webhooks/782664118128869406/ic-s9pKgnQlWWesoQnwBQ9cgpum7EPH_Z64W3sEUJVUZ7WoF1zvX353tLKC123s-Ss3s
+    for item in response.items.iter() {
+        println!("item: {:#?}", item);
+    }
+    println!("Done processing response.");
+}
+
+fn get_url(from: &Duration, to: &Duration, tag: &str, max_items: u8, key: &str) -> String {
     format!("https://api.stackexchange.com/2.2/questions?\
         page=1&\
-        pagesize=2&\
         order=asc&\
         sort=creation&\
-        tagged=sentry&\
         site=stackoverflow&\
+        pagesize={}&\
         fromdate={}&\
         todate={}&\
-        q={}&\
+        tagged={}&\
         key={}",
+        max_items,
         from.as_secs(),
         to.as_secs(),
-        query,
+        tag,
         key)
 }
 
